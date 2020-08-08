49bd8a6971d929e251060c4b1e49dd1d1909f7b2
Etu Vincent <69142396+Ojimaojo-cell@users.noreply.github.com>
2020-08-08 01:51:43 +0100

    Add files via upload
    My Script, python

---
+++ Team-mendel, Vincent_01.py
@@ -0,0 +1,9 @@
+#python 3.7.1
+
+print ("Name: Etu, Vincent Ojimaojo")
+print("Email: ojimaojo95@gmail.com")
+print ("Language used: python")
+print ("Biostack: Genomics")
+print ("Slack Username: @Vincent_01")
+
+   
