2a2373a93c8fc5340ea838046d5f8eac5df09ce8
Etu Vincent <69142396+Ojimaojo-cell@users.noreply.github.com>
2020-08-08 01:48:52 +0100

    Delete Team-mendel, Vincent_01.py
    Mistake

--- Team-mendel, Vincent_01.py
+++
@@ -1,9 +0,0 @@
-#python 3.7.1
-
-print ("Name: Etu, Vincent Ojimaojo")
-print("Email: ojimaojo95@gmail.com")
-print ("Language used: python")
-print ("Biostack: Genomics")
-print ("Slack Username: @Vincent_01")
-
-   
